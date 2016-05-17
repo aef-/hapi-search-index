fc3bb3ee679b2c55851ad4b0b962d00e3488bbf5
Adrian Fraiha <aef@fastmail.com>
2016-05-17 15:11:36 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+
+exports.register = function(server, options, next) {
+  const searchindex = require('search-index');
+  searchIndex(options, function(err, si) {
+    server.expose('search', si);
+    next();
+  });
+};
+
+exports.register.attributes = {
+  pkg: require('./package.json')
+};
